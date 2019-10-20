738954fb51de731a8240ee0a51f5aaecd56716cd
youngMaster46 <kaplin95@inbox.ru>
2019-10-20 11:45:32 +0300

    lalal

--- src/components/Vk/Vk.jsx
+++ src/components/Vk/Vk.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import * as axios from 'axios';
 
 const Vk = (props) => {
-    const onSubmit = () => {
-        axios.get('')
+    const onLogin = () => {
+        axios.get(`https://oauth.vk.com/authorize?
+        client_id=7177067&display=page&
+        redirect_uri=https://oauth.vk.com/blank.html&scope=4098&response_type=token&v=5.52`)
+        .then(response => console.log(response))
     }
     return <div>
         <h1>vk.com</h1>
-        <button onClick={onSubmit}  >submit</button>
+        <button onClick={onLogin}  >submit</button>
     </div>
 }
 
